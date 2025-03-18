d9a78b74363b03013110ab95a5432fa5c003a265
Paytriot <102317067+PaytriotP@users.noreply.github.com>
2025-03-18 17:15:21 +0000

    Update HeroComponent.tsx
    Start Now button updation.

--- src/components/landing-page/HeroComponent.tsx
+++ src/components/landing-page/HeroComponent.tsx
@@ -1,7 +1,7 @@
 import LottiePlayer from '@/components/LottiePlayer';
 import { orange } from '@material-ui/core/colors';
 import { Button } from '@nextui-org/react';
-import { PopupButton } from '@typeform/embed-react';
+// import { PopupButton } from '@typeform/embed-react';
 
 import Link from 'next/link';
 import headerAnimationJson from 'public/animation/landing-page/header.json';
@@ -28,23 +28,23 @@ export default function HeroComponent() {
               <p className="p-18 mb-5 text-center text-lg-start">
               Paytriot now offers a monthly fee for businesses looking to start processing or switching. 
               </p>
-{/*               <Link
+              <Link
                 href="/SignupForm"
                 className="btn btn-primary"
                 role="button"
                 data-bs-toggle="button"
               >
                 Start Now
-              </Link> */}
+              </Link>
               {/* <Link href="/contact-us" style={{ textDecoration: 'none' }}>
                 <Button rounded size="xl" color="warning">
                   Start Now
                 </Button> }
 
               </Link> */}
-                  <PopupButton id="uJkBAIlB" size={60} style={buttonStyle} className="my-button">
+{/*                   <PopupButton id="uJkBAIlB" size={60} style={buttonStyle} className="my-button">
                     Start Now
-                  </PopupButton>
+                  </PopupButton> */}
             </div>
           </div>
           <div className="col-lg-6 d-flex justify-content-center">

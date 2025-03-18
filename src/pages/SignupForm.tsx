4671138b4d9a166d609b0193de86609d5bf91db0
Paytriot <102317067+PaytriotP@users.noreply.github.com>
2025-03-18 15:14:53 +0000

    Update SignupForm.tsx

--- src/pages/SignupForm.tsx
+++ src/pages/SignupForm.tsx
@@ -75,9 +75,8 @@ const onSubmit = async (data: FormValues) => {
       return;
     }
 
-    const response = await fetch("https://script.google.com/macros/s/AKfycbwGszRHnke5oeYhbulemJtRnZ8A2HsinNt4hXBSl6z9S6uags2ScCZFJmhgPGr9o1WQ/exec", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbwGszRHnke5oeYhbulemJtRnZ8A2HsinNt4hXBSl6z9S6uags2ScCZFJmhgPGr9o1WQ/exec" {
       method: "POST",
-      mode: "no-cors",
       headers: {
         "Content-Type": "application/json",
       },

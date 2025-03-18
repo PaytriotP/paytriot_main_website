dc357120274a529a96a1f2cd67cbfd2973cc97d3
Paytriot <102317067+PaytriotP@users.noreply.github.com>
2025-03-18 15:57:55 +0000

    Update submitForm.ts

--- src/pages/api/submitForm.ts
+++ src/pages/api/submitForm.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     try {
-      const response = await fetch('https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec', {
+      const response = await fetch('https://script.google.com/macros/s/AKfycbwz_NyXLZim6XM_51QWO894KEeSreQKnlu837Hub7g80ZQJCBjWplm05eHQB-31TedI/exec', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',

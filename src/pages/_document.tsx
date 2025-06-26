7aaa1ff332439a9f17761af849b03e1237d19216
Paytriot <102317067+PaytriotP@users.noreply.github.com>
2025-06-26 18:27:58 +0100

    Update _document.tsx
    Add Tone.js for web audio functionalities

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {CssBaseline.flush()}
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.49/Tone.min.js"></script>
         </Head>
         <body>
           <Main />

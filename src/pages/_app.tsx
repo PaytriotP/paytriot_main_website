821b2d2ef5d4fd29846317ebba4f8d2c44bfd04a
Paytriot <102317067+PaytriotP@users.noreply.github.com>
2025-08-11 00:13:55 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -42,7 +42,6 @@ export default function App({ Component, pageProps }: AppProps) {
           <TrackingScript/>
           <NavigationMenu />
           <Component {...pageProps} />
-          <BotpressChat />
           <CallToAction />
           <Footer />
         </main>
